f2b01cb3b3047a2a8d66e5ce31edf947596e1cc8
JadedDragoon <bugger.others@gmail.com>
2018-05-10 08:05:29 -0500

    added deletion of database if found

--- randsim.bat
+++ randsim.bat
@@ -1,2 +1,3 @@
-@echo off
+@ECHO OFF
+IF EXIST "%~dp0\database.sqlite" DEL "%~dp0\database.sqlite"
 "%~dp0\node.exe" "%~dp0\index.js" %1 %2 %3 %4
--- randsim.sh
+++ randsim.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+if [ -f $SCRIPTPATH/database.sqlite ]; then rm $SCRIPTPATH/database.sqlite; fi
 $SCRIPTPATH/node $SCRIPTPATH/index.js $1 $2 $3 $4
